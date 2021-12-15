1c558a56104b1aadd2aab88e73d4836a53ae1b07
Morok0 <95218742+Morok0@users.noreply.github.com>
2021-12-15 19:54:04 +0300

    Add files via upload

---
+++ Pr_7_zad_1/Pr_7_zad_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Pr_7_zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string line = Console.ReadLine();
+            Console.WriteLine("Исходная строка: " + line);
+            Console.WriteLine("Введите символ x: ");
+            char x = char.Parse(Console.ReadLine());
+            Console.WriteLine("Введите символ y: ");
+            char y = char.Parse(Console.ReadLine());
+
+            int countX = line.Where(a => a == x).Count();//это просто счётчик, который считает символы
+            int countY = line.Where(a => a == y).Count();
+            string result = "Символ '{0}' встречается чаще в строке.";
+            if (line.Contains((char) x)|| line.Contains((char)y))
+            {
+                if (countX > countY)//строчные и заглавные символы, это два разных симввола даже если это одна буква
+                Console.WriteLine(result, x);
+            else if (countY > countX)
+                Console.WriteLine(result, y);
+            else if (countY == countX )
+                Console.WriteLine("Символы '{0}' и '{1}' встречаются одинаковое количество раз в строке.", x, y);
+
+            }
+            else Console.WriteLine("Таких символов нет");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pr_7_zad_2/Pr_7_zad_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+namespace Pr_7_zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int buf = 0;
+            string str = "";
+            string[] s;
+            str = Console.ReadLine();
+            s = str.Split();
+            for (int i = 0; i < s.Length; i++)
+            {
+                buf = s[i].Length;
+                for (int j = 0; j < s[i].Length; j++)
+                {
+                    if (char.IsUpper(s[i][j]))
+                    {
+                        for (int z = 0; z < s[i].Length; z++)
+                        {
+                            if (s[i][z] != ',')
+                                    if (s[i][z] != '.')
+                                                Console.Write(s[i][z]);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+
+            }
+               
+        }
+    }
+}
